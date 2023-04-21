62f920d9b3a3e92d20eef93cce654ce586c1f784
Zachari Price <ZachariPrice3@gmail.com>
2023-04-20 20:46:07 -0400

    Selection Statement Exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I LOVE math!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is okay");
+                    break;
+                case "pe":
+                    Console.WriteLine("I am super competitive in PE");
+                    break;
+                case "english":
+                    Console.WriteLine("English is chellenging");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+            }
         }
     }
 }
